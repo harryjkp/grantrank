0303b4f67111c8e69ddb91819271b248b6c01057
harryjkp <harryjkp@gmail.com>
2023-05-02 19:31:23 +0300

    Dummy API

--- backend/api/lambda_function.py
+++ backend/api/lambda_function.py
@@ -1,12 +1,19 @@
 import json
 
+pr = [0.,0.13494152,0.11470029,0.09749525,0.08287096,0.07044032,0.05987427,0.05089313,0.04325916,0.18677036,0.15875473]
+
 def lambda_handler(event, context):
-    name = event.get('name', 'please enter a name in the query string')
-    message = f'Hello, {name}!'
+    node = event.get('node')
+
+    if node:
+        response = {'nodes':{node:pr[node]},'edges' : {}}
+    else:
+        response = {'nodes': {'node': [9, 10, 1, 2, 3], 'pagerank': pr}, 'edges': {'sources': [1, 2, 9, 10], 'targets': [2, 3, 10, 1], 'weights': [1, 1, 1, 1]}}
+
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body': json.dumps({'message': message})
+        'body': response
     }
