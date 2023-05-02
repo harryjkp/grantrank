1cc046c98b2054c738941c6ae057973e81da5d4c
harryjkp <harryjkp@gmail.com>
2023-05-02 17:17:41 +0300

    Adjust message

--- backend/api/lambda_function.py
+++ backend/api/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    name = event.get('name', ', please enter a name in the query string')
+    name = event.get('name', 'please enter a name in the query string')
     message = f'Hello, {name}!'
     return {
         'statusCode': 200,
