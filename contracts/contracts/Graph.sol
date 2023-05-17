26e9737c6fe8ddce74286fc573500c19a536f438
harryjkp <harryjkp@gmail.com>
2023-05-17 16:02:49 +0300

    Added frontend boilerplate and re-arranged repo

---
+++ contracts/contracts/Graph.sol
@@ -0,0 +1,103 @@
+pragma solidity 0.8;
+
+contract Graph {
+    mapping(address => uint) public addressToId;
+
+    Node[] public nodes;
+
+
+    struct Node {
+        address _address;
+        bool isHuman;
+        string name;
+        string description;
+        string image;
+        uint budget;  //TODO: Make the budget is delayed by 3 months
+       
+
+    }
+
+    uint[] public sourceIds;
+    uint[] public targetIds;
+    uint[] public weights;
+
+    uint public numNodes;
+    uint public numEdges;
+    uint public historicalGranted;
+
+
+
+    function addNode(bool isHuman, string memory name, string memory description,string memory image, uint budget ) public {
+        address _address = msg.sender;
+        nodes.push(Node(_address, isHuman, name, description,image, budget));
+        addressToId[_address] = numNodes;
+        numNodes++;
+    }
+
+    function addEdge(address recipient, uint weight) public {
+        uint senderId = addressToId[msg.sender];
+        uint recipientId = addressToId[recipient];
+        sourceIds.push(senderId);
+        targetIds.push(recipientId);
+        weights.push(weight);
+    }
+
+    function addMultipleEdges(address[] memory recipients, uint[] memory weightsToAdd) public {
+        uint senderId = addressToId[msg.sender];
+        for (uint i = 0; i < recipients.length; i++) {
+            uint recipientId = addressToId[recipients[i]];
+            sourceIds.push(senderId);
+            targetIds.push(recipientId);
+            weights.push(weightsToAdd[i]);
+        }
+
+    }
+
+
+    uint public constant DECIMALS = 1000000000000000000;
+
+    function getPageRank(address _address) public view returns (uint) {
+        return 1;
+    }
+
+    function getMyPageRank() public view returns (uint) {
+        return getPageRank(msg.sender);
+    }
+
+    mapping(address => uint) public addressToWithdrawalAmount;
+
+    function fund() public payable {
+        totalBudget += msg.value;
+        historicalBudget += msg.value;
+    }
+
+    uint public totalBudget;
+
+    uint public monthlyBudget;
+    uint public historicalBudget; 
+    function withdraw(uint amount) public { // Accounts are entitled to funds proportional to their pagerank. They should not be able to withdraw more than their budget: min(M*p/sum(p), budget)
+        uint amount = addressToWithdrawalAmount[msg.sender];     
+
+
+        uint available = monthlyBudget*getMyPageRank()/DECIMALS - addressToWithdrawalAmount[msg.sender];
+        
+        require(amount <= available, "Insufficient funds");
+        addressToWithdrawalAmount[msg.sender] += amount;
+        totalBudget -= amount;
+
+        payable(msg.sender).transfer(amount);
+    }
+
+    
+    // Function to reset withdrawal amounts. Can only be called by Chainlink automation
+
+    function resetWithdrawalAmounts() public {
+        monthlyBudget = totalBudget;
+        require(false, "TODO: Make sure this can only be called by Chainlink automation");
+
+        // addressToWithdrawalAmount = new mapping(address => uint); // TODO: Figure out how to reset a mapping https://stackoverflow.com/questions/48045784/solidity-setting-a-mapping-to-empty
+
+    }
+
+
+}
---
+++ contracts/scripts/deploy.js
@@ -0,0 +1,53 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+
+async function main() {
+
+
+  const Graph = await hre.ethers.getContractFactory("Graph");
+  const graph = await Graph.deploy();
+
+  await graph.deployed();
+
+  console.log(`Graph deployed to ${graph.address}`)
+
+  // no need to verify on localhost or hardhat
+  if (network.config.chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
+    console.log(`Waiting for block confirmation...`)
+    await graph.deployTransaction.wait(5)
+
+    console.log('Verifying contract...')
+    try {
+      run('verify:verify', {
+        address: graph.address,
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+  
+
+//   console.log(
+//     `graph-address=${graph.address}`// `Graph deployed to ${graph.address}`
+//   );
+
+//     //wait for 5 block transactions to ensure deployment before verifying
+
+//     await graph.deployTransaction.wait(5);
+  
+// }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
