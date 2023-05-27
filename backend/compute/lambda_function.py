8d6cabf81ab455e7253db90a1af23c4380d07444
harryjkp <harryjkp@gmail.com>
2023-05-27 20:31:39 +0300

    Update lambda_function.py

--- backend/compute/lambda_function.py
+++ backend/compute/lambda_function.py
@@ -57,8 +57,9 @@ truncated_df = df[df['sources'].isin(top_results['node']) & df['targets'].isin(t
 
 def lambda_handler(event, context):
     """If a node is given, return the pagerank of that node. Otherwise, return the pagerank of all nodes. """
-    node = event['queryStringParameters'].get('node')
+    node = event.get('queryStringParameters',{}).get('node')
     if node:
+        node = int(node)
         message = {'pagerank': pr[node]}
     else:
         message = {'nodes':top_results.to_dict(orient='list'),'edges' : truncated_df.to_dict(orient='list')}
